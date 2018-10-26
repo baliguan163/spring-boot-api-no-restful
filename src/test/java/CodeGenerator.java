b5d3d0af04d0f467161cacd62f980f5079d21768
baliguan163 <baliguan163@163.com>
2018-10-26 16:37:06 +0800

    修改

--- src/test/java/CodeGenerator.java
+++ src/test/java/CodeGenerator.java
@@ -37,12 +37,9 @@ public class CodeGenerator {
     private static final String DATE = new SimpleDateFormat("yyyy-MM-dd").format(new Date());//@date
 
     public static void main(String[] args) {
-//        genCode("tian_mao_goods");
-//        genCode("yangxian_news");
-//         genCode("baliguan_news");
-
-//        genCode("choiceness_goods_list");
-
+//        genCode("goods_choiceness");
+//        genCode("goods_groupbuying");
+//        genCode("goods_quality");
         //genCodeByCustomModelName("输入表名","输入自定义Model名称");
     }
 
